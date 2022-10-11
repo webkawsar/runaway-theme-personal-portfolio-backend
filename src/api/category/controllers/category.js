baf3d20f781614887d543436c06ab1460fe35f98
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-10-11 18:31:56 +0600

    added slug wise category post and apply custom pagination category post

--- src/api/category/controllers/category.js
+++ src/api/category/controllers/category.js
@@ -6,4 +6,43 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::category.category');
+module.exports = createCoreController('api::category.category', ({strapi}) => {
+
+    return {
+        async findOne(ctx) {
+
+            const {query: {pagination: {page, pageSize}, populate } } = ctx;
+            const {id: slug} = ctx.params;
+            const categories = await strapi.entityService.findMany('api::category.category', {
+                populate: [...populate],
+                filters: { slug },
+                limit: 1
+              });
+
+            if(!categories) return ctx.notFound('Posts is not found');
+            
+            // created pagination system
+            const category = categories[0];
+            const posts = category.posts;
+            const pageNumber = page ? +page : 1;
+            const postsPerPage = pageSize ? pageSize : process.env.CATEGORY_POSTS_PER_PAGE;
+            const totalPosts = posts.length;
+            const paginatedPosts = posts.splice((pageNumber - 1) * postsPerPage, postsPerPage);
+
+            const response = {
+                ...category,
+                posts: paginatedPosts,
+                pagination: {
+                    currentPage: pageNumber,
+                    previousPage: pageNumber - 1,
+                    nextPage: pageNumber + 1,
+                    hasPreviousPage: pageNumber > 1,
+                    hasNextPage: pageNumber * postsPerPage < totalPosts,
+                    lastPage: Math.ceil(totalPosts / postsPerPage),
+                }
+            }
+
+            return response;
+        }
+    }
+});
