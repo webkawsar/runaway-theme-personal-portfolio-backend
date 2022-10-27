faacab2fb2e995dc3371514e74facfeab0e35c6e
webkawsar <web.kawsarahmed@gmail.com>
2022-10-27 09:44:27 +0600

    added ecommerce audio app api in my portfolio server

---
+++ src/api/ecommerce-category/controllers/ecommerce-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ecommerce-category controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::ecommerce-category.ecommerce-category');
---
+++ src/api/ecommerce-category/routes/ecommerce-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ecommerce-category router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ecommerce-category.ecommerce-category');
